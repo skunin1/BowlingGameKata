4075ce12645cad51c4b728b22006dc5b4f05aec6
acta-non-verba <vikas.joshi@live.com>
2021-11-12 17:59:55 +0530

    Sprint 3 test cases passing (Need more refactor)

--- src/main/java/com/agile/BowlingGameKata.java
+++ src/main/java/com/agile/BowlingGameKata.java
@@ -18,17 +18,18 @@ public class BowlingGameKata {
         Frame frame = new Frame();
         frame.setRoll1(roll1);
         frame.setRoll2(roll2);
-        if (scoreBoard.size() > 1 && isSpare(scoreBoard.getLast())) {
+        if (scoreBoard.size() > 1 && isSpare(scoreBoard.getLast().getRoll1(), scoreBoard.getLast().getRoll2())) {
             scoreBoard.getLast().setFrameScore(getPreviousFrameScore() + roll1);
             frame.setFrameScore(getPreviousFrameScore() + roll1 + roll2);
-        } else if (scoreBoard.size() > 1 && isStrike(scoreBoard.getLast())
-                && isStrike(scoreBoard.get(scoreBoard.size() - 2))) {
-            scoreBoard.get(scoreBoard.size()-2).setFrameScore(scoreBoard.get(scoreBoard.size() - 2).getFrameScore() + roll1);
-           
-            scoreBoard.getLast().setFrameScore(getPreviousFrameScore()+roll1 + roll1 + roll2);
+        } else if (scoreBoard.size() > 1 && isStrike(scoreBoard.getLast().getRoll1())
+                && isStrike(scoreBoard.get(scoreBoard.size() - 2).getRoll1())) {
+            scoreBoard.get(scoreBoard.size() - 2)
+                    .setFrameScore(scoreBoard.get(scoreBoard.size() - 2).getFrameScore() + roll1);
+
+            scoreBoard.getLast().setFrameScore(getPreviousFrameScore() + roll1 + roll1 + roll2);
 
             frame.setFrameScore(getPreviousFrameScore() + roll1 + roll2);
-        } else if (scoreBoard.size() > 1 && isStrike(scoreBoard.getLast())) {
+        } else if (scoreBoard.size() > 1 && isStrike(scoreBoard.getLast().getRoll1())) {
             scoreBoard.getLast().setFrameScore(getPreviousFrameScore() + roll1 + roll2);
             frame.setFrameScore(getPreviousFrameScore() + roll1 + roll2);
         } else {
@@ -39,15 +40,27 @@ public class BowlingGameKata {
     }
 
     public int calculateFrameScore(int... frameRolls) {
-        if (validateNoOfFrames()
-                && eachFramecanKnockMax10Pins(frameRolls[0], frameRolls.length > 1 ? frameRolls[1] : 0))
-            return addFrameToScoreBoard(frameRolls[0], frameRolls.length > 1 ? frameRolls[1] : 0);
-        else
+        if (validateNoOfFrames() 
+                && scoreBoard.size() == 9 && isStrike(frameRolls[0])) {
+            int scoreTillFrame10 = addFrameToScoreBoard(frameRolls[0],0);
+            scoreBoard.getLast().setFrameScore(scoreTillFrame10 + frameRolls[1]+ frameRolls[2]);
             return scoreBoard.getLast().getFrameScore();
+        } else if (validateNoOfFrames()
+                && eachFramecanKnockMax10Pins(frameRolls[0], frameRolls.length > 1 ? frameRolls[1] : 0)
+                && scoreBoard.size() == 9 && isSpare(frameRolls[0], frameRolls.length > 1 ? frameRolls[1] : 0)) {
+            int scoreTillFrame10 = addFrameToScoreBoard(frameRolls[0], frameRolls.length > 1 ? frameRolls[1] : 0);
+            scoreBoard.getLast().setFrameScore(scoreTillFrame10 + frameRolls[2]);
+            return scoreBoard.getLast().getFrameScore();
+        } else if (validateNoOfFrames()
+                && eachFramecanKnockMax10Pins(frameRolls[0], frameRolls.length > 1 ? frameRolls[1] : 0)) {
+            return addFrameToScoreBoard(frameRolls[0], frameRolls.length > 1 ? frameRolls[1] : 0);
+        } else {
+            return scoreBoard.getLast().getFrameScore();
+        }
     }
 
     private int getPreviousFrameScore() {
-        if (scoreBoard.size() > 0)
+        if (!scoreBoard.isEmpty())
             return scoreBoard.getLast().getFrameScore();
         else
             return 0;
@@ -61,12 +74,12 @@ public class BowlingGameKata {
         return scoreBoard;
     }
 
-    public boolean isSpare(Frame frame) {
-        return (frame.getRoll1() < 10 && (frame.getRoll1() + frame.getRoll2() == 10));
+    public boolean isSpare(int roll1, int roll2) {
+        return (roll1 < 10 && (roll1 + roll2 == 10));
     }
 
-    public boolean isStrike(Frame frame) {
-        return frame.getRoll1() == 10;
+    public boolean isStrike(int roll1) {
+        return roll1 == 10;
     }
 
 }
--- src/test/java/com/agile/BowlingGameKataTest.java
+++ src/test/java/com/agile/BowlingGameKataTest.java
@@ -67,7 +67,7 @@ public class BowlingGameKataTest{
         frame1.setRoll2(7);
         frames.add(frame1);
         //Act
-        boolean actual=kata.isSpare(frame1);
+        boolean actual=kata.isSpare(frame1.getRoll1(),frame1.getRoll2());
         boolean expected=true;
         //Assert
         assertEquals(expected, actual);
@@ -112,7 +112,7 @@ public class BowlingGameKataTest{
         frame1.setRoll1(10);
         frames.add(frame1);
         //act
-        boolean actual=kata.isStrike(frame1);
+        boolean actual=kata.isStrike(frame1.getRoll1());
         boolean expected=true;
         //Assert
         assertEquals(expected, actual);
@@ -216,6 +216,149 @@ public class BowlingGameKataTest{
         kata.calculateFrameScore(frame10.getRoll1(), frame10.getRoll2());
         int[] actualScoresArray = kata.getScoreBoard().stream().mapToInt(Frame::getFrameScore).toArray();
         //Assert
+        assertArrayEquals(expectedScoresArray, actualScoresArray);
+    }
+
+    @Test
+    public void shouldAddExtraBonusRolltoTenthFrameIfTenthFrameHasSpare()
+    {
+        LinkedList<Frame> frames=new LinkedList<Frame>();
+
+        Frame frame1=new Frame();
+        frame1.setRoll1(1);
+        frame1.setRoll2(4);
+        frames.add(frame1);
+
+        Frame frame2=new Frame();
+        frame2.setRoll1(3);
+        frame2.setRoll2(5);
+        frames.add(frame2);
+
+        Frame frame3=new Frame();
+        frame3.setRoll1(4);
+        frame3.setRoll2(6);
+        frames.add(frame3);
+
+        Frame frame4=new Frame();
+        frame4.setRoll1(2);
+        frame4.setRoll2(0);
+        frames.add(frame4);
+
+        Frame frame5=new Frame();
+        frame5.setRoll1(10);
+        frames.add(frame5);
+
+        Frame frame6=new Frame();
+        frame6.setRoll1(4);
+        frame6.setRoll2(3);
+        frames.add(frame6);
+        
+
+        Frame frame7=new Frame();
+        frame7.setRoll1(10);
+        frames.add(frame7);
+        
+
+        Frame frame8=new Frame();
+        frame8.setRoll1(10);
+        frames.add(frame8);       
+
+        Frame frame9=new Frame();
+        frame9.setRoll1(5);
+        frame9.setRoll2(3);
+        frames.add(frame9);        
+
+        Frame frame10=new Frame();
+        frame10.setRoll1(7);
+        frame10.setRoll2(3);
+        frames.add(frame10);
+
+        //Act
+        int[] expectedScoresArray = {5,13,25,27,44,51,76,94,102,122};
+
+        kata.calculateFrameScore(frame1.getRoll1(), frame1.getRoll2());
+        kata.calculateFrameScore(frame2.getRoll1(), frame2.getRoll2());
+        kata.calculateFrameScore(frame3.getRoll1(), frame3.getRoll2());
+        kata.calculateFrameScore(frame4.getRoll1(), frame4.getRoll2());
+        kata.calculateFrameScore(frame5.getRoll1());
+        kata.calculateFrameScore(frame6.getRoll1(), frame6.getRoll2());
+        kata.calculateFrameScore(frame7.getRoll1());
+        kata.calculateFrameScore(frame8.getRoll1());
+        kata.calculateFrameScore(frame9.getRoll1(), frame9.getRoll2());
+        kata.calculateFrameScore(frame10.getRoll1(), frame10.getRoll2(),10);
+        int[] actualScoresArray = kata.getScoreBoard().stream().mapToInt(Frame::getFrameScore).toArray();
+        //Assert         
+        assertArrayEquals(expectedScoresArray, actualScoresArray);
+    }
+     
+    @Test
+    public void shouldAddExtraBonusRolltoTenthFrameIfTenthFrameHasStrike()
+    {
+        LinkedList<Frame> frames=new LinkedList<Frame>();
+
+        Frame frame1=new Frame();
+        frame1.setRoll1(1);
+        frame1.setRoll2(4);
+        frames.add(frame1);
+
+        Frame frame2=new Frame();
+        frame2.setRoll1(3);
+        frame2.setRoll2(5);
+        frames.add(frame2);
+
+        Frame frame3=new Frame();
+        frame3.setRoll1(4);
+        frame3.setRoll2(6);
+        frames.add(frame3);
+
+        Frame frame4=new Frame();
+        frame4.setRoll1(2);
+        frame4.setRoll2(0);
+        frames.add(frame4);
+
+        Frame frame5=new Frame();
+        frame5.setRoll1(10);
+        frames.add(frame5);
+
+        Frame frame6=new Frame();
+        frame6.setRoll1(4);
+        frame6.setRoll2(3);
+        frames.add(frame6);
+        
+
+        Frame frame7=new Frame();
+        frame7.setRoll1(10);
+        frames.add(frame7);
+        
+
+        Frame frame8=new Frame();
+        frame8.setRoll1(10);
+        frames.add(frame8);       
+
+        Frame frame9=new Frame();
+        frame9.setRoll1(5);
+        frame9.setRoll2(3);
+        frames.add(frame9);        
+
+        Frame frame10=new Frame();
+        frame10.setRoll1(10);
+        frames.add(frame10);
+
+        //Act
+        int[] expectedScoresArray = {5,13,25,27,44,51,76,94,102,122};
+
+        kata.calculateFrameScore(frame1.getRoll1(), frame1.getRoll2());
+        kata.calculateFrameScore(frame2.getRoll1(), frame2.getRoll2());
+        kata.calculateFrameScore(frame3.getRoll1(), frame3.getRoll2());
+        kata.calculateFrameScore(frame4.getRoll1(), frame4.getRoll2());
+        kata.calculateFrameScore(frame5.getRoll1());
+        kata.calculateFrameScore(frame6.getRoll1(), frame6.getRoll2());
+        kata.calculateFrameScore(frame7.getRoll1());
+        kata.calculateFrameScore(frame8.getRoll1());
+        kata.calculateFrameScore(frame9.getRoll1(), frame9.getRoll2());
+        kata.calculateFrameScore(frame10.getRoll1(),5,5);
+        int[] actualScoresArray = kata.getScoreBoard().stream().mapToInt(Frame::getFrameScore).toArray();
+        //Assert         
         assertArrayEquals(expectedScoresArray, actualScoresArray);
     }
 
